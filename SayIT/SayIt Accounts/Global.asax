bce897899ffb423b5064d0a41d8c93c0319e30d5
Michał Ciernia <ziemniak1996@gmail.com>
2019-06-13 17:14:44 +0200

    Added Owin Auth

---
+++ SayIT/SayIt Accounts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SayIt_Accounts.MvcApplication" Language="C#" %>
---
+++ SayIT/SayIt Accounts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SayIt_Accounts.Startup))]
+namespace SayIt_Accounts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
